f9ffda8a321634f36b3d3bfc99b0afaa0266708f
Jim Eliot <benjaminjeliot@gmail.com>
2018-10-20 13:48:19 +0100

    Tests for Fortran identifier parser
    Also tweaks to Fortran identifier parser

--- include/namelist_def.hpp
+++ include/namelist_def.hpp
@@ -9,6 +9,7 @@ namespace parser {
 
 namespace x3 = boost::spirit::x3;
 
+using x3::lexeme;
 using x3::lit;
 using x3::string;
 using x3::int_;
@@ -17,11 +18,11 @@ using x3::alpha;
 using x3::alnum;
 
 //! Parser for Fortran identifier name
-auto const fortran_identifier = (alpha >> *(alnum | char_("_")));
+auto const fortran_identifier = lexeme[(alpha >> *(alnum | char_("_")))];
 
 //! Parser for Fortran namelist header
 //! A dollar sign '$' followed by an identifier
-auto const namelist_def = lit('$') >> fortran_identifier;
+auto const namelist_def = lexeme[lit('$') >> fortran_identifier];
 
 }  // namespace nmlcpp
 }  // namespace parser
---
+++ test/ParserFortranIdentifierTest.cpp
@@ -0,0 +1,56 @@
+//
+// test/ParserFortranIdentifierTest.cpp
+//
+
+#include <gtest/gtest.h>
+#include "ast.hpp"
+#include "namelist_def.hpp"
+
+TEST(ParserFortranIdentifierTest, Initialisation) {
+  using boost::spirit::x3::ascii::space;
+  using boost::spirit::x3::phrase_parse;
+  bool r;
+
+  std::string::const_iterator iter, end;
+
+
+  std::string alphas = "alphas";
+  iter = alphas.cbegin();
+  end = alphas.cend();
+  r = phrase_parse(iter, end, nmlcpp::parser::fortran_identifier, space);
+  EXPECT_TRUE(r);
+  EXPECT_EQ(end, iter);
+
+  std::string alphas_nums_underscores = "my_variable_1";
+  iter = alphas_nums_underscores.cbegin();
+  end = alphas_nums_underscores.cend();
+  r = phrase_parse(iter, end, nmlcpp::parser::fortran_identifier, space);
+  EXPECT_TRUE(r);
+  EXPECT_EQ(end, iter);
+
+  std::string contains_space = "identifier_1 another_identifier";
+  iter = contains_space.cbegin();
+  end = contains_space.cend();
+  r = phrase_parse(iter, end, nmlcpp::parser::fortran_identifier, space);
+  EXPECT_TRUE(r);
+  // Check that we have only consumed 'identifier_1' from the input
+  EXPECT_EQ('a', *iter);
+
+  std::string leading_num = "1alphas";
+  iter = leading_num.cbegin();
+  end = leading_num.cend();
+  r = phrase_parse(iter, end, nmlcpp::parser::fortran_identifier, space);
+  EXPECT_FALSE(r);
+
+  std::string leading_underscore = "_alphas";
+  iter = leading_underscore.cbegin();
+  end = leading_underscore.cend();
+  r = phrase_parse(iter, end, nmlcpp::parser::fortran_identifier, space);
+  EXPECT_FALSE(r);
+
+  std::string leading_dollar = "$leading_dollar";
+  iter = leading_dollar.cbegin();
+  end = leading_dollar.cend();
+  r = phrase_parse(iter, end, nmlcpp::parser::fortran_identifier, space);
+  EXPECT_FALSE(r);
+}
