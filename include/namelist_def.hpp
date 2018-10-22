0ef5ccc397f02495ecda6b6298e0d991051e7ed6
Jim Eliot <benjaminjeliot@gmail.com>
2018-10-22 07:31:43 +0100

    Added parsers for doubly and singly quoted strings
    Also initial tests

--- include/namelist_def.hpp
+++ include/namelist_def.hpp
@@ -9,13 +9,15 @@ namespace parser {
 
 namespace x3 = boost::spirit::x3;
 
+using x3::alpha;
+using x3::alnum;
+using x3::attr;
+using x3::char_;
+using x3::double_;
+using x3::int_;
 using x3::lexeme;
 using x3::lit;
 using x3::string;
-using x3::int_;
-using x3::char_;
-using x3::alpha;
-using x3::alnum;
 
 //! Parser for Fortran identifier name
 auto const fortran_identifier = lexeme[(alpha >> *(alnum | char_("_")))];
@@ -23,6 +25,15 @@ auto const fortran_identifier = lexeme[(alpha >> *(alnum | char_("_")))];
 //! Parser for Fortran namelist header
 //! A dollar sign '$' followed by an identifier
 auto const namelist_def = lexeme[lit('$') >> fortran_identifier];
+
+// TODO: Extend parser to deal with a double quote escaped with a double quote
+//! Parser for a double quoted string
+auto const double_quoted_string = lexeme['"' >> *(("\"\"" >> attr('"')) | (char_ - '"')) >> '"'];
+
+//! Parser for a single quoted string
+auto const single_quoted_string = lexeme['\'' >> *(char_ - '\'') >> '\''];
+
+auto const single_object = double_quoted_string | single_quoted_string | int_ | double_;
 
 }  // namespace nmlcpp
 }  // namespace parser
---
+++ test/ParserStringTest.cpp
@@ -0,0 +1,42 @@
+//
+// test/ParserStringTest.cpp
+//
+
+#include <gtest/gtest.h>
+#include "ast.hpp"
+#include "namelist_def.hpp"
+
+TEST(ParserStringTest, FriendlyStrings) {
+  using boost::spirit::x3::ascii::space;
+  using boost::spirit::x3::phrase_parse;
+  bool r;
+  std::string::const_iterator iter, end;
+
+  std::string hello = "\"nospaces\"";
+  iter = hello.cbegin();
+  end = hello.cend();
+  r = phrase_parse(iter, end, nmlcpp::parser::double_quoted_string, space);
+  EXPECT_TRUE(r);
+  EXPECT_EQ(end, iter);
+
+  std::string string_with_space = "\"string with spaces\"";
+  iter = string_with_space.cbegin();
+  end = string_with_space.cend();
+  r = phrase_parse(iter, end, nmlcpp::parser::double_quoted_string, space);
+  EXPECT_TRUE(r);
+  EXPECT_EQ(end, iter);
+}
+
+TEST(ParserStringTest, AwkwardStrings) {
+  using boost::spirit::x3::ascii::space;
+  using boost::spirit::x3::phrase_parse;
+  bool r;
+  std::string::const_iterator iter, end;
+
+  std::string string_with_quotes = "\"String \"\"containing\"\" quote\"";
+  iter = string_with_quotes.cbegin();
+  end = string_with_quotes.cend();
+  r = phrase_parse(iter, end, nmlcpp::parser::double_quoted_string, space);
+  EXPECT_TRUE(r);
+  EXPECT_EQ(end, iter);
+}
